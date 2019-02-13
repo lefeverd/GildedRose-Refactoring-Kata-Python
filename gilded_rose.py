5953ac22683c00784362efb6d65a5adf9c157a64
David Lefever <lefever.d@gmail.com>
2019-02-13 22:09:10 +0100

    Added Backstage Pass behavior.

--- gilded_rose.py
+++ gilded_rose.py
@@ -10,8 +10,24 @@ class AgedBrieBehavior:
         item.sell_in = item.sell_in - 1
 
 
+class BackstagePassTAFKAL80ETCBehavior:
+    def update_quality(self, item):
+        if item.sell_in <= 0:
+            item.quality = 0
+        elif item.sell_in <= 5:
+            item.quality = min(50, item.quality + 3)
+        elif item.sell_in <= 10:
+            item.quality = min(50, item.quality + 2)
+        else:
+            item.quality = min(50, item.quality + 1)
+        item.sell_in = item.sell_in - 1
+
+
 BEHAVIORS = {
-    "Aged Brie": AgedBrieBehavior(),
+    "Aged Brie":
+    AgedBrieBehavior(),
+    "Backstage passes to a TAFKAL80ETC concert":
+    BackstagePassTAFKAL80ETCBehavior(),
 }
 
 
