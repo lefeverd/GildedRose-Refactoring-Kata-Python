8f028837385fdc3688c42e877f995d83d4132840
David Lefever <lefever.d@gmail.com>
2019-02-13 22:08:35 +0100

    Improved Aged Brie behavior (decrease sell in at the end).

--- gilded_rose.py
+++ gilded_rose.py
@@ -3,11 +3,11 @@
 
 class AgedBrieBehavior:
     def update_quality(self, item):
-        item.sell_in = item.sell_in - 1
-        if item.sell_in < 0:
+        if item.sell_in <= 0:
             item.quality = min(50, item.quality + 2)
         else:
             item.quality = min(50, item.quality + 1)
+        item.sell_in = item.sell_in - 1
 
 
 BEHAVIORS = {
